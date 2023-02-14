0cf837e3bb26652f634a4a0cdcd1d4a07b511e0f
RedoX <pseudokawaii@gmail.com>
2023-02-14 13:19:47 +0600

    update web/log_config.py

--- web/log_config.py
+++ web/log_config.py
@@ -67,7 +67,7 @@ def configure_logger() -> None:
     # Configure loguru (again) if gunicorn is not used
     logger.configure(handlers=[{
         "sink": sys.stderr,
-        "level": LOG_LEVEL,
+        "level": "INFO",
         "format": "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
         "colorize": True
     }, {
