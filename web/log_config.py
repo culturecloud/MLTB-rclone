bd43fa8c46a948b999c2807463fdb79e9b8a24a2
RedoX <pseudokawaii@gmail.com>
2023-02-14 13:26:40 +0600

    update web/log_config.py

--- web/log_config.py
+++ web/log_config.py
@@ -39,7 +39,7 @@ class StubbedGunicornLogger(Logger):
         # Configure loguru before gunicorn starts logging
         logger.configure(handlers=[{
             "sink": sys.stderr,
-            "level": LOG_LEVEL,
+            "level": "INFO",
             "format": "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
             "colorize": True
         }, {
